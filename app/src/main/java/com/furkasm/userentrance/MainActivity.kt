bee84aa0696a203130f5b633f096271da437a0b7
== <=>
2023-02-01 16:53:12 +0300

    intentler uygulandı

--- app/src/main/java/com/furkasm/userentrance/MainActivity.kt
+++ app/src/main/java/com/furkasm/userentrance/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.furkasm.userentrance
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.furkasm.userentrance.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +14,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val loginButton = findViewById<Button>(R.id.saveButton)
+
+      // val button = findViewById<Button>(R.id.signupView)
+
+        loginButton.setOnClickListener{
+            val intent = Intent(this,SignUpPage ::class.java)
+
+            startActivity(intent)
+        }
     }
 
 
+    fun signupClicked(view : View){
+        val intent = Intent(this,SignUpPage :: class.java)
+
+        startActivity(intent)
+    }
+    /*
+    fun buttonClicked(view: View){
+        if (textEmail.text.text)
+    }
+    */
 
 }
--- app/src/main/java/com/furkasm/userentrance/SignUpPage.kt
+++ app/src/main/java/com/furkasm/userentrance/SignUpPage.kt
@@ -1,11 +1,32 @@
 package com.furkasm.userentrance
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.DrawableCompat.inflate
+import android.view.View.inflate
+import android.widget.Button
+import com.furkasm.userentrance.databinding.ActivityMainBinding.inflate
+import com.furkasm.userentrance.databinding.ActivitySignUpPageBinding
+import com.furkasm.userentrance.databinding.ActivitySignUpPageBinding.inflate
 
 class SignUpPage : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_page)
+        binding = ActivitySignUpPageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val saveButton = findViewById<Button>(R.id.saveButton)
+
+        saveButton.setOnClickListener{
+            val intent = Intent(this,MainActivity :: class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
